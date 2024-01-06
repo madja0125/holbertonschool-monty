acd4b7356aa7ded357f776c12ccf59fafdfa302c
madja0125 <dejesusmiguel788@gmail.com>
2024-01-06 00:26:36 +0000

    adding proto

--- monty.h
+++ monty.h
@@ -43,5 +43,9 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 void pint(stack_t **stack, unsigned int line_number);
-
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void tokenize_and_execute(char *line, unsigned int line_number, stack_t **stack);
 #endif
